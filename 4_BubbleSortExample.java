21691310cc166c8ef52b561f422f93fdd23046d2
VPikul <pikul.v@cmu.ac.th>
2024-12-03 22:15:00 +0700

    add sort files for lecture 4

---
+++ 4_BubbleSortExample.java
@@ -0,0 +1,35 @@
+public class BubbleSortExample {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        // Outer loop for passes
+        for (int i = 0; i < n - 1; i++) {
+            // Inner loop for comparing adjacent elements
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1] if they are in the wrong order
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("\nSorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
